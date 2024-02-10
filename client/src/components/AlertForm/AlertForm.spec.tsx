9dfdf8c1c27f02afee3fa037686f2b2976d2a3d4
Andrew Casarsa <acasarsa@gmail.com>
2024-02-10 15:03:20 -0500

    changed file structure to add a client and server dir. and replaced template with chakra ui **Big looking commit but mainly from those 2 things deleting and re-adding a bunch of files.

---
+++ client/src/components/AlertForm/AlertForm.spec.tsx
@@ -0,0 +1,1 @@
+import AlertForm from './AlertForm'
---
+++ client/src/components/AlertForm/AlertForm.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+interface AlertFormProps {}
+const AlertForm: React.FC<AlertFormProps> = () => {
+  return <div>Form here</div>
+}
+
+export default AlertForm

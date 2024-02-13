85a14b63f0fac8e36208462f0995aac6b71065a4
Andrew Casarsa <acasarsa@gmail.com>
2024-02-12 22:21:26 -0500

    add react router and a bell icon to show there are triggered alerts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,58 @@
 import './App.css'
-import { useColorMode, Button } from '@chakra-ui/react'
+import {
+  useColorMode,
+  Button,
+  Box,
+  Flex,
+  Link as ChakraLink,
+  LinkProps as ChakraLinkProps,
+} from '@chakra-ui/react'
 import AlertList from './components/AlertList/AlertList'
 import AlertForm from './components/AlertForm/AlertForm'
+import { AlertNotificationTag } from './components/base/AlertNotificationTag'
 import { useAlerts } from './context/AlertContext'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link as ReactRouterLink,
+} from 'react-router-dom'
 
 function App() {
   const { alerts } = useAlerts()
   const { colorMode, toggleColorMode } = useColorMode()
+  // const navigate = useNavigate()
 
   return (
-    <div className="App">
-      <Button m={4} onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-      </Button>
-      <AlertList items={alerts} />
-      <AlertForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Button m={4} onClick={toggleColorMode}>
+          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+        </Button>
+        <Flex
+          justifyContent="space-around"
+          my={4}
+          borderBottom="1px"
+          borderColor="pink.200"
+          pb={4}
+        >
+          <ChakraLink as={ReactRouterLink} to="/form">
+            Alert Form
+          </ChakraLink>
+          <Flex>
+            <ChakraLink as={ReactRouterLink} to="/alerts" mr={1}>
+              Triggered Alerts
+            </ChakraLink>
+            {alerts.length > 0 && <AlertNotificationTag />}
+          </Flex>
+        </Flex>
+        <Routes>
+          <Route path="/alerts" element={<AlertList items={alerts} />} />
+          <Route path="/form" element={<AlertForm />} />
+          <Route path="/" element={<AlertForm />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
---
+++ client/src/components/base/AlertNotificationTag.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Tag } from '@chakra-ui/react'
+import { BellIcon } from '@chakra-ui/icons'
+
+export const AlertNotificationTag = () => {
+  return (
+    // <Tag size="lg" colorScheme="teal" borderRadius="full">
+    <BellIcon boxSize={4} color="pink" />
+    // </Tag>
+  )
+}
+
+// export default AlertNotificationTag
